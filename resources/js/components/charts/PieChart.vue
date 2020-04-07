d52c78db7bc7901ffd1f1f1446a8f470d4243012
Eoan O Dea <odeaeoan@gmail.com>
2020-04-07 12:20:51 +0100

    Improved charts tand fixed bugs with them

--- resources/js/components/charts/PieChart.vue
+++ resources/js/components/charts/PieChart.vue
@@ -1,128 +1,63 @@
 <script>
-import { Pie } from "vue-chartjs";
-import { mapGetters } from "vuex";
+  import { Pie } from "vue-chartjs";
 
-export default {
-  name: "pie-chart",
-  extends: Pie,
-  data: () => ({
-    colours: [
-      "#ffc246",
-      "#448aff",
-      "#d81b60",
-      "#ff6488",
-      "#52a3ff",
-      "#ffba44",
-      "#1bd893",
-      "#55f5c0",
-      "#b3d876",
-      "#bd90e8",
-      "#d44a20",
-      "#274c01",
-      "#3a52dc",
-      "#ceda2e",
-      "#903fb2",
-      "#4e6d6f"
-    ],
-    chartData: {
-      labels: [],
-      datasets: [],
-      options: { responsive: true }
-    }
-  }),
-  created() {
-    const app = this;
-    if (app.enrolments.length < 1) {
-      app.$store.dispatch("enrolment/loadEnrolments").then(() => {
-        app.populateChart();
-      });
-    } else app.populateChart();
-    // this.renderChart(app.chartData)
-  },
-  methods: {
-    populateChart() {
-      console.log("runing!!");
-      const { enrolments, colours } = this;
-      let { chartData } = this;
-      
-
-      enrolments.map((enrolment, i) => {
-        const enrolmentColor =
-          colours.length > i
-            ? colours[Math.floor(Math.random() * colours.length)]
-            : colours[i];
-
-        if(chartData.datasets.filter(data => data.label === enrolment.status).length < 1) {
-            chartData.datasets.push({
+  export default {
+    name: "pie-chart",
+    props: ['enrolments'],
+    extends: Pie,
+    data: () => ({
+      colours: [
+        "#ffc246",
+        "#448aff",
+        "#d81b60",
+        "#ff6488",
+        "#52a3ff",
+        "#ffba44"
+      ],
+      chartData: {
+        labels: [],
+        datasets: [
+          {
+            data: [],
+            backgroundColor: []
+          }
+        ],
+        options: { responsive: true }
+      }
+    }),
+    created() {
+      this.populateChart()
+    },
+    methods: {
+      populateChart() {
+        const { enrolments, colours } = this;
+        let { chartData } = this;
+        let status = [];
+        enrolments.map((enrolment, i) => {
+          if (status.filter(data => data.label === enrolment.status).length < 1) {
+            status.push({
               label: enrolment.status,
-              backgroundColor: enrolmentColor,
-              data: [1]
-            })
+              count: 1
+            });
+          } else {
+            const index = status.findIndex((e) => e.label === enrolment.status);
+            let count = status[index].count;
+            count = count + 1;
+            
+            status[index] = { label: status[index].label, count: count };
+          }
+        });
+        status.map((dat, i) => {
+          chartData.labels.push(dat.label.replace('_', ' '));
+          chartData.datasets[0].data.push(dat.count);
+          chartData.datasets[0].backgroundColor.push(colours[i])
+        });
+        
+        this.$nextTick(() => {      
+            this.renderChart(chartData);
 
-            chartData.labels.push(enrolment.status)
-        } else {
-          chartData.datasets.map(data => {
-              if (data.label === enrolment.status) {
-                const num = (data.data[data.data.length -1] + 1)
-                console.log('num!', data.data.length)
-                return data.data.push(num)
-                
-              }
-          });
-        }
-      })
-
-      // months.map(month => {
-      //   enrolments
-      //     .filter((dat, i) => (dat.enrolments.length > 0 && i < 5))
-      //     .map((dat, i) => {
-      //       const enrolmentColor =
-      //         colours.length > i
-      //           ? colours[Math.floor(Math.random() * colours.length)]
-      //           : colours[i];
-  
-      //       console.log(colours.length > i && enrolmentColor);
-      //       const enrolments = dat.enrolments.filter(enrolment => {
-      //         return enrolment.date.includes(
-      //           `-${month > 9 ? month : "0" + month}-`
-      //         );
-      //       });
-
-      //       chartData.datasets.filter(data => data.label === dat.title).length <
-      //       1
-      //         ? chartData.datasets.push({
-      //             label: dat.title,
-      //             borderColor: enrolmentColor,
-      //             backgroundColor: "#ff000000",
-      //             data: [enrolments.length]
-      //           })
-      //         : chartData.datasets.map(data => {
-      //             if (data.label === dat.title) {
-      //               console.log(
-      //                 "pushing enrolments!",
-      //                 enrolments.length,
-      //                 dat.title,
-      //                 month
-      //               );
-
-      //               return data.data.push(enrolments.length);
-      //             }
-      //           });
-      //       // }
-      //     });
-
-      //   chartData.labels.push(month);
-      // });
-
-      this.renderChart(chartData);
+        })
+      }
     }
-  },
-  computed: {
-    ...mapGetters({
-      enrolments: "enrolment/enrolments",
-      loading: "enrolment/loading",
-      error: "enrolment/error"
-    })
-  }
-};
+  };
 </script>
--- resources/js/views/Dashboard.vue
+++ resources/js/views/Dashboard.vue
@@ -7,14 +7,16 @@
             <md-card-header>
               <div class="md-title">Popular Courses this year</div>
             </md-card-header>
-            <line-chart />
+            <line-chart v-if="courses.length > 0" :courses="courses" />
+            <loading-indicator v-else />
         </md-card>
         
         <md-card class="md-layout-item md-medium-size-45 md-small-size-45 md-xsmall-size-100">
             <md-card-header>
               <div class="md-title">Enrolment Status</div>
             </md-card-header>
-            <pie-chart />
+            <pie-chart v-if="enrolments.length > 0" :enrolments="enrolments" />
+            <loading-indicator v-else />
         </md-card>
       
       </div>
@@ -23,17 +25,41 @@
   import Vue from 'vue'
   import LineChart from './../components/charts/LineChart'
   import PieChart from './../components/charts/PieChart'
+  import { mapGetters } from "vuex";
+  import LoadingIndicator from './../components/LoadingIndicator'
 
   export default {
     name: 'dashboard',
     components: {
       LineChart,
-      PieChart
+      PieChart,
+      LoadingIndicator
+    },
+    created() {
+      const app = this;
+      /**
+       * Load enrolments from the store if they do not already exist
+       */
+      if (app.enrolments.length < 1) {
+        app.$store.dispatch("enrolment/loadEnrolments")
+      }
+      /**
+       * Load courses from the store if they do not already exist
+       */
+      if (app.courses.length < 1) {
+        app.$store.dispatch("course/loadCourses")
+      }
     },
     data() {
       return {
 
       }
+    },
+    computed: {
+      ...mapGetters({
+          enrolments: "enrolment/enrolments",
+          courses: "course/courses"
+      })
     }
 }
 </script>
